3d43e2d1ef62f985640ebc8b1ed3427a8bbd723c
Diego <dgutierrez@zagile.com>
2022-10-13 18:26:27 -0500

    first commit

---
+++ src/main/java/com/example/testspringsleuth/TestSpringSleuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringsleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringSleuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringSleuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringsleuth/TestSpringSleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringsleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringSleuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
